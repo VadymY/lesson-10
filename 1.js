4298c41a5089960601dca61918d2cba196394938
Vadym Yashchenko <vadym.yashchenko@gmail.com>
2020-06-15 10:50:50 +0300

    1

---
+++ 1.js
@@ -0,0 +1,51 @@
+/**
+ * Задача 1.
+ *
+ * Напишите имплементацию функции Function.prototype.bind().
+ *
+ * Наша собственная функция bind() должна задавать контекст выполнения целевой функции,
+ * и возвращать новую, привязанную версию целевой функции.
+ *
+ * Функция bind() должна обладать следующими параметрами:
+ * - Первый параметр — это функция, контекст выполнения которой мы хотим привязать;
+ * - Второй параметр — это объект (не массив), в контексте которого нужно вызывать функцию из первого параметра;
+ * - Третий и все остальные параметры — это аргументы для вызова функции из первого параметра.
+ *
+ * Генерировать ошибки если:
+ * - Первый параметр не является типом function;
+ * - Второй параметр не является типом object;
+ * - Второй параметр является массивом.
+ *
+ * Условия:
+ * - Использовать встроенную функцию Function.prototype.bind() запрещено.
+ */
+
+// Решение
+
+function bind(cb, obj, ...arg) {
+    if (typeof(cb) !== "function"){
+        throw new Error("it is not a function");
+    }
+    if (typeof(obj) !== "object"){
+        throw new Error("it is not an object");
+    }
+    if (Array.isArray(obj)){
+        throw new Error("it is an array");
+    }
+    this.name = obj.name;
+    return () => cb(...arg);
+}
+
+function getName(greeting, message) {
+    return `${greeting} ${message} ${this.name}.`;
+}
+
+const user = {name: 'Walter', getName};
+const oliver = {name: 'Oliver'};
+
+const boundedGetName = bind(getName, oliver, 'Hello!', 'I am');
+
+console.log(user.getName('Hello!', 'My name is')); // Hello! My name is Walter.
+console.log(boundedGetName()); // Hello! I am Oliver.
+
+exports.bind = bind;
---
+++ 4.js
@@ -0,0 +1,77 @@
+/**
+ * Задача 4.
+ *
+ * Напишите функции compose(), которая в качестве аргументов принимает неограниченное количество функций.
+ *
+ * При запуске compose() последовательно запускает коллбек-функции из аргументов.
+ *
+ * Важно: compose() выполняет коллбек-функции из аргументов НАЧИНАЯ С КОНЦА.
+ *
+ * Каждая коллбек-функция из цепочки в качестве своего аргумента принимает то, что возвращает предыдущая коллбек-функция.
+ * То есть возвращаемое значение каждой коллбек-функции из цепочки
+ * становится доступным из параметра следующей коллбек-функции в цепочке.
+ *
+ * Функция compose() возвращает ещё одну функцию с одним параметром.
+ * Значение, переданное этой функции в качестве аргумента должно стать
+ * параметром первой коллбек-функции в цепочке выполнения функции compose().
+ *
+ * Итого, подпись функции compose: `compose(f, g)(x) = f(g(x))`.
+ *
+ * Генерировать ошибки если:
+ * - Любой из аргументов не является функцией;
+ * - Любая функция из аргументов не вернула значение.
+ *
+ * Заметка:
+ * Если функции, которая является возвращаемым значением compose()
+ * не передать в качестве аргумента какое-либо значение, генерировать ошибку не нужно.
+ */
+
+// Решение
+function compose(...cb){
+
+    this.cbs = [];
+
+    for (let  i = 0; i < cb.length; i++){
+        this.cbs.push(cb[i]);
+    }
+
+    return (arg) => {
+        let next_arg = arg;
+          if (typeof(next_arg) !== "undefined"){
+            next_arg = String(next_arg);
+        }
+
+        for (let i = this.cbs.length - 1;  i >= 0; i--){
+            if (typeof(this.cbs[i]) !== "function"){
+                throw new Error('it is not function');
+            }
+
+            next_arg = this.cbs[i](next_arg);
+
+            if (typeof(next_arg) === "undefined"){
+                throw new Error('function did not return a parameter');
+            }
+        }
+
+        return next_arg;
+    }
+}
+
+const result1 = compose(
+    prevResult => prevResult + 'o',
+    prevResult => prevResult + 'l',
+    prevResult => prevResult + 'l',
+    prevResult => prevResult + 'e'
+)('h');
+const result2 = compose(
+    prevResult => prevResult + 'o',
+    prevResult => prevResult + 'l',
+    prevResult => prevResult + 'l',
+    prevResult => prevResult + 'e',
+    () => 'h',
+)();
+
+console.log(result1); // 'hello'
+console.log(result2); // 'hello'
+
+exports.compose = compose;
